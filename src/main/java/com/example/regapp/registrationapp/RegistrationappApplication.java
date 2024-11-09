257bd51e4fb3bc2e48ca3f1cab9763f3a903b7ef
sagar241990 <sagar241990@gmail.com>
2024-11-09 20:25:27 +0530

    Initial commit

---
+++ src/main/java/com/example/regapp/registrationapp/RegistrationappApplication.java
@@ -0,0 +1,13 @@
+package com.example.regapp.registrationapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrationappApplication.class, args);
+	}
+
+}
